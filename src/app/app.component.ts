545a66d0c019f6cf4c1f4697c3102789a31c792a
balocenm <balocenm@gmail.com>
2019-12-26 09:18:39 -0500

    feat(master): implementación de primera demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reto-tech';
+
+  model: any = {};
+  model2: any = {};
+
+  msg: string = '';
+  msgActive: boolean = false;
+  hiddeUpdate: boolean = true;
+  employees = [
+    { 'name': 'Fazt', position: 'manager', email: 'balocen' },
+    { 'name': 'Juan', position: 'Designer', email: 'balocen' },
+    { 'name': 'Pedro', position: 'Programmer', email: 'balocen' },
+  ];
+  myValue;
+  addEmployee() {
+    this.employees.push(this.model);
+    this.msg = 'campo agregado';
+    this.msgActive = true;
+  }
+
+  deleteEmployee(i) {
+    const answer = confirm('estas seguro de querer eliminarlo?');
+    if (answer) {
+      this.employees.splice(i, 1);
+      this.msg = 'campo eliminado';
+      this.msgActive = true;
+    }
+  }
+
+  updateEmployee() {
+    let i = this.myValue;
+    for (let j = 0; j < this.employees.length; j++) {
+      if (i === j) {
+        this.employees[i] = this.model2;
+        this.msg = 'campo actualizado';
+        this.msgActive = true;
+        this.model2 = {};
+      }
+    }
+  }
+
+  editEmployee(i) {
+    this.hiddeUpdate = false;
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
+  }
+
+  closeAlert() {
+    this.msg = '';
+    this.msgActive = false;
+  }
 }
