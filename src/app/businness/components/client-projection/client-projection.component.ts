1530b88e0c896b1fe05dc03a5919bbc3e6ccbac5
balocenl <braya.alocen@inkafarmadigital.pe>
2019-12-30 13:54:57 -0500

    feat(master): implements fecha probable de muerte

--- src/app/businness/components/client-projection/client-projection.component.ts
+++ src/app/businness/components/client-projection/client-projection.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IClient } from '../../models/client.interface';
+import { IClient, IClientDead } from '../../models/client.interface';
 import { DataDbService } from '../../service/data-db.service';
 
 @Component({
@@ -9,39 +9,115 @@ import { DataDbService } from '../../service/data-db.service';
 })
 export class ClientProjectionComponent implements OnInit {
 
-  rango = [{
-    ONE: '1_10',
-    ELEVEN: '11_20',
-    TWENTY: '21_30',
-    THIRTY: '31_40',
-    FORTY: '41_50',
-    FIFTY: '51_60',
-    SIXTY: '61_70',
-    SEVENTY: '71_80',
-    EIGHTY: '81_90',
-    NINETY: '91_99',
-  }];
-
+  list = [
+    {
+      id: 'range_0_10',
+      value: 3.23,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_11_20',
+      value: 0.65,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_21_30',
+      value: 1.21,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_31_40',
+      value: 1.84,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_41_50',
+      value: 4.31,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_51_60',
+      value: 9.69,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_61_70',
+      value: 18.21,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_71_80',
+      value: 27.28,
+      AverageLifeHope: 80
+    },
+    {
+      id: 'range_81_more',
+      value: 33.58,
+      AverageLifeHope: 80
+    },
+  ];
 
   listClients: IClient[];
+  listClientDead: IClientDead[];
+
   average: number;
   result: number;
+  range: string;
+  edadMuerte: number;
+  fechaProbableMuerte: string;
   constructor(private dbData: DataDbService) { }
 
   ngOnInit() {
     this.dbData.items.subscribe(value => {
-      value.map(a => {
-        this.calcularRangoEdad(a.age);
+      value.map((a, i) => {
+        const ranges = this.rango(a.age);
+        this.listClientDead = value;
+         this.listClientDead[i].dead = this.deahtProbability(ranges, a.age, a.date);
       });
-      this.listClients = value;
     });
   }
 
+  rango(age: number) {
+    if (age >= 0 && age <= 10) {
+      return '0_10';
+    } else if (age >= 11 && age <= 20) {
+      return '11_20';
+    } else if (age >= 21 && age <= 30) {
+      return '21_30';
+    } else if (age >= 31 && age <= 40) {
+      return '31_40';
+    } else if (age >= 41 && age <= 50) {
+      return '41_50';
+    } else if (age >= 51 && age <= 60) {
+      return '51_60';
+    } else if (age >= 61 && age <= 70) {
+      return '61_70';
+    } else if (age >= 71 && age <= 80) {
+      return '71_80';
+    } else if (age >= 81 && age <= 90) {
+      return '81_90';
+    } else if (age >= 91 && age <= 99) { return '91_99'; }
+  }
 
-  calcularRangoEdad(age: string) {
-    this.rango.map(value => {
-       
+  deahtProbability(range: string, age: number, date: string) {
+    const f = this.list.find(a => {
+      return a.id === ('range_' + range);
     });
+
+    const añosPorVivir = f.AverageLifeHope - age;
+
+    if (añosPorVivir > 0) {
+      this.edadMuerte = (añosPorVivir * (100 - f.value) / 100) + age;
+    }
+
+    const vec = date.split('-');
+    // tslint:disable-next-line: radix
+    const fecha = new Date(parseInt(vec[0]), parseInt(vec[1]), parseInt(vec[2]));
+    fecha.setFullYear(fecha.getFullYear() + this.edadMuerte);
+    this.fechaProbableMuerte = fecha.getFullYear() + '-' + fecha.getMonth() + '-' + fecha.getDate();
+
+    return this.fechaProbableMuerte;
+
   }
 
 }
--- src/app/businness/models/client.interface.ts
+++ src/app/businness/models/client.interface.ts
@@ -6,50 +6,16 @@ export interface IClient {
     date: string;
 }
 
+export interface IClientDead {
+    id?: string;
+    name: string;
+    lastName: string;
+    age: number;
+    date: string;
+    dead: string;
+}
+
 export class Config {
     minDate: string;
     maxDate: string;
-}
-// tslint:disable-next-line: no-namespace
-export namespace Api {
-    // interface Range [
-    //     ONE: '1_10',
-    //     ELEVEN: '11_20';
-    //     TWENTY: '21_30';
-    //     THIRTY: '31_40';
-    //     FORTY: '41_50';
-    //     FIFTY: '51_60';
-    //     SIXTY: '61_70';
-    //     SEVENTY: '71_80';
-    //     EIGHTY: '81_90';
-    //     NINETY: '91_99';
-    // ]
-
-    interface DeathProbability {
-        range_0_10: 3.23;
-        range_11_20: 0.65;
-        range_21_30: 1.21;
-        range_31_40: 1.84;
-        range_41_50: 4.31;
-        range_51_60: 9.69;
-        range_61_70: 18.21;
-        range_71_80: 27.28;
-        range_81_more: 33.58;
-
-    }
-
-    interface averageLifeHope {
-        range_0_10: 80;
-        range_11_20: 80;
-        range_21_30: 80;
-        range_31_40: 80;
-        range_41_50: 80;
-        range_51_60: 80;
-        range_61_70: 84;
-        range_71_80: 88;
-        range_81_more: 95;
-    }
-
-
-
 }
